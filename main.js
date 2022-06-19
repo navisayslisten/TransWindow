9ba5b004594cb0cd970afd9e94f1c89fdd6dd111
Navi Says Listen <taylor.navi.stone@protonmail.com>
2022-06-18 21:56:42 -0600

    Use ipcRenderer to send messages from backend

--- main.js
+++ main.js
@@ -1,5 +1,58 @@
 const { app, BrowserWindow } = require('electron')
-const { chatServer } = require('./chatWrapper.js')
+const { ChatClient} = require('twitch-chat-client');
+const { RefreshableAuthProvider, StaticAuthProvider } = require('twitch-auth');
+const { promises } = require('fs');
+
+const tokenFile = './tokens.json';
+
+async function chatServer(window) {
+  const channel = process.env.CHANNEL;
+  const clientId = process.env.CLIENTID;
+  const clientSecret = process.env.CLIENTSECRET;
+
+  const tokens = await promises.readFile(tokenFile, 'utf8');
+  const tokenData = JSON.parse(tokens);
+  const authProvider = new RefreshableAuthProvider(
+    new StaticAuthProvider(clientId, tokenData.accessToken),
+    {
+      clientSecret,
+      refreshToken: tokenData.refreshToken,
+      expiry: tokenData.expiryTimestamp === null ? null : new Date(tokenData.expiryTimestamp),
+      onRefresh: async ({ accessToken, refreshToken, expiryDate }) => {
+        const newTokenData = {
+          accessToken,
+          refreshToken,
+          expiryTimestamp: expiryDate === null ? null : expiryDate.getTime()
+        };
+        await promises.writeFile(tokenFile, JSON.stringify(newTokenData, null, 4), 'utf8')
+      }
+    }
+  );
+
+  const chatClient = new ChatClient(authProvider, {
+    channels: [channel],
+  });
+
+  await chatClient.connect();
+
+  chatClient.onMessage((channel, user, message) => {
+    if (message === '!ping') {
+      window.webContents.send('ping', `pong to ${user}`);
+    }
+  });
+
+  chatClient.onSub((channel, user) => {
+    window.webContents.send('sub', `Thanks to @${user} for subscribing to the channel!`);
+  });
+
+  chatClient.onResub((channel, user, subInfo) => {
+    window.webContents.send('resub', `Thanks to @${user} for subscribing to the channel for a total of ${subInfo.months} months!`);
+  });
+
+  chatClient.onSubGift((channel, user, subInfo) => {
+    window.webContents.send('giftsub', `Thanks to ${subInfo.gifter} for gifting a subscription to ${user}!`);
+  });
+}
 
 let win;
 
@@ -11,18 +64,17 @@ async function createWin() {
     transparent: true,
     frame: false,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false,
     }
   })
 
-  win.loadFile('index.html').then(() => console.log('index loaded'))
-  await chatServer();
+  win.loadFile('index.html').then(() => console.log('Loaded index.html'))
+  await chatServer(win).then(() => console.log('Connected to chat server'))
 }
 
 app.on('ready', () => {
-  if (process.platform === 'win32') {
-    app.setAppUserModelId('TWITCH.COM/DIGGITYSC')
-  }
+    app.setAppUserModelId('TWITCH.COM/DIGGITYSC');
 })
 
 app.whenReady().then(async () => {
--- renderer.js
+++ renderer.js
@@ -1,14 +1,28 @@
-const { Notification } = require('electron')
+const { ipcRenderer } = require('electron')
 
-function showNotification(title, body) {
-  const options = {
-    title,
-    body,
-    silent: false,
-    timeoutType: 'default'
-  }
-  new Notification(options).show();
-  console.log(`${title}: ${body}`);
-}
 
-exports.showNotification = showNotification;
+let messages = document.querySelector('#messages');
+
+ipcRenderer.on('ping', (event, ping) => {
+  let message = document.createElement('div');
+  message.textContent = `${ping}`;
+  messages.appendChild(message);
+})
+
+ipcRenderer.on('sub', (event, body) => {
+  let message = document.createElement('div');
+  message.textContent = `${body}`;
+  messages.appendChild(message);
+})
+
+ipcRenderer.on('resub', (event, body) => {
+  let message = document.createElement('div');
+  message.textContent = `${body}`;
+  messages.appendChild(message);
+})
+
+ipcRenderer.on('giftsub', (event, body) => {
+  let message = document.createElement('div');
+  message.textContent = `${body}`;
+  messages.appendChild(message);
+})

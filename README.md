87e992c2f263ddc5dd2a15a9284c519fa37f4bd7
Navi Says Listen <taylor.navi.stone@protonmail.com>
2024-10-20 15:02:30 -0700

    Update project libraries and auth provider

--- README.md
+++ README.md
@@ -7,11 +7,12 @@ The future is now, but you can't even see it!
 ## SETUP
 Create a .env file with the following variables:
 ```
-CHANNEL=
-CLIENTID=
-CLIENTSECRET=
-ACCESS_TOKEN=
-REFRESHTOKEN=
+CHANNEL=justyourchannelnamenotURL
+USERID=12345678
+CLIENTID=somesecretid
+CLIENTSECRET=someclientid
+ACCESS_TOKEN=someaccesstoken
+REFRESHTOKEN=somerefreshtoken
 EXPIRYTIMESTAMP=0
 ```
 
@@ -20,6 +21,8 @@ Follow the instructions on the Twitch API documentation to get your access token
 - https://dev.twitch.tv/console/apps - Set up a new application
 - https://d-fischer.github.io/twitch-chat-client/docs/examples/basic-bot.html - basic auth setup instructions
 
-Could use yarn, but I found yarn kinda sucks for debugging the Electron app.
+Once your access and refresh token values are set in the .env file, please also set them in the tokens.json file
 
-So `yarn install` or `npm install` the app and then `yarn start` or `npm start`.
+`yarn install`
+
+`yarn start`
--- main.js
+++ main.js
@@ -1,95 +1,89 @@
-const { app, BrowserWindow } = require('electron')
-const { ChatClient } = require('@twurple/chat');
-const { RefreshableAuthProvider, StaticAuthProvider } = require('@twurple/auth');
-const { promises } = require('fs');
+const {app, BrowserWindow} = require('electron')
+const {ChatClient} = require('@twurple/chat');
+const {RefreshingAuthProvider} = require('@twurple/auth');
+const {promises, writeFile} = require('fs');
 
 const tokenFile = './tokens.json';
 
+
 async function chatServer(window) {
-  const channel = process.env.CHANNEL;
-  const clientId = process.env.CLIENTID;
-  const clientSecret = process.env.CLIENTSECRET;
+    const channel = process.env.CHANNEL;
+    const clientId = process.env.CLIENTID;
+    const clientSecret = process.env.CLIENTSECRET;
 
-  const tokens = await promises.readFile(tokenFile, 'utf8');
-  const tokenData = JSON.parse(tokens);
-  const authProvider = new RefreshableAuthProvider(
-    new StaticAuthProvider(clientId, tokenData.accessToken),
-    {
-      clientSecret,
-      refreshToken: tokenData.refreshToken,
-      expiry: tokenData.expiryTimestamp === null ? null : new Date(tokenData.expiryTimestamp),
-      onRefresh: async ({ accessToken, refreshToken, expiryDate }) => {
-        const newTokenData = {
-          accessToken,
-          refreshToken,
-          expiryTimestamp: expiryDate === null ? null : expiryDate.getTime()
-        };
-        await promises.writeFile(tokenFile, JSON.stringify(newTokenData, null, 4), 'utf8')
-      }
-    }
-  );
+    const tokens = await promises.readFile(tokenFile, 'utf8');
+    const tokenData = JSON.parse(tokens);
+    const authProvider = new RefreshingAuthProvider({
+            clientId,
+            clientSecret
+        }
+    )
+    authProvider.onRefresh(async newTokenData => await writeFile('./tokens.json', JSON.stringify(newTokenData, null, 4), 'utf-8'))
+    // Add intents depending on what your app is doing
+    await authProvider.addUserForToken(tokenData, ['chat'])
 
-  const chatClient = new ChatClient({
-    authProvider: authProvider,
-    channels: [channel],
-  });
+    const chatClient = new ChatClient({
+        authProvider: authProvider,
+        channels: [channel],
+    });
 
-  await chatClient.connect();
+    await chatClient.connect();
 
-  chatClient.onMessage((channel, user, message) => {
-    if (message === '!ping') {
-      window.webContents.send('ping', `pong to ${user}`);
-    }
-  });
+    chatClient.onMessage((channel, user, message) => {
+        if (message === '!ping') {
+            window.webContents.send('ping', `pong to ${user}`);
+        }
+    });
 
-  chatClient.onSub((channel, user) => {
-    window.webContents.send('sub', `Thanks to @${user} for subscribing to the channel!`);
-  });
+    chatClient.onSub((channel, user) => {
+        window.webContents.send('sub', `Thanks to @${user} for subscribing to the channel!`);
+    });
 
-  chatClient.onResub((channel, user, subInfo) => {
-    window.webContents.send('resub', `Thanks to @${user} for subscribing to the channel for a total of ${subInfo.months} months!`);
-  });
+    chatClient.onResub((channel, user, subInfo) => {
+        window.webContents.send('resub', `Thanks to @${user} for subscribing to the channel for a total of ${subInfo.months} months!`);
+    });
 
-  chatClient.onSubGift((channel, user, subInfo) => {
-    window.webContents.send('giftsub', `Thanks to ${subInfo.gifter} for gifting a subscription to ${user}!`);
-  });
+    chatClient.onSubGift((channel, user, subInfo) => {
+        window.webContents.send('giftsub', `Thanks to ${subInfo.gifter} for gifting a subscription to ${user}!`);
+    });
 }
 
 let win;
 
 async function createWin() {
-  require('dotenv').config()
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    transparent: true,
-    frame: false,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    }
-  })
+    require('dotenv').config()
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        transparent: true,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    })
 
-  win.loadFile('index.html').then(() => console.log('Loaded index.html'))
-  await chatServer(win).then(() => console.log('Connected to chat server'))
+    win.loadFile('index.html').then(() => console.log('Loaded index.html'))
+    await chatServer(win).then(() => console.log('Connected to chat server'))
 }
 
 app.on('ready', () => {
-    app.setAppUserModelId('TWITCH.COM/DIGGITYSC');
+    const channel = process.env.CHANNEL;
+    app.setAppUserModelId(`TWITCH.COM/${channel}`);
 })
 
 app.whenReady().then(async () => {
-  await createWin();
+    await createWin();
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'win32') {
-    app.quit()
-  }
+    if (process.platform !== 'win32') {
+        app.quit()
+    }
 })
 
 app.on('activate', async () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    await createWin()
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        await createWin()
+    }
 })
